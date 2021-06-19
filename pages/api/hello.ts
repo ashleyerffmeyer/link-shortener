a23cf9ad32c9d9141bd813fbaedcf92606979482
ashleyerffmeyer <ashley.erffmeyer@gmail.com>
2021-06-19 16:17:13 -0500

    up to setting up vercel

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { connectToDatabase } from "./_connector";
-
-export default async (req, res) => {
-  await connectToDatabase();
-
-  res.status(200).json({ name: "John Doe" });
-};
